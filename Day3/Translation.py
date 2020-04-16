b03e31d1994b38f80f3a7e6bd8ff04fd42b830b3
khoaihust <58268745+khoaihust@users.noreply.github.com>
2020-04-16 21:51:51 +0700

    Image Transformations

--- Day3/Translation.py
+++ Day3/Translation.py
@@ -4,7 +4,8 @@ import imutils
 import cv2
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required = True, help = 'Path to the image')
+ap.add_argument('-i', '--image', required = True, 
+                help = 'Path to the image')
 args = vars(ap.parse_args())
 
 image = cv2.imread(args['image'])
