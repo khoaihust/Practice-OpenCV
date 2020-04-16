d48c8a5b2b7aaf7bd8801c70dadcc66a058832a8
khoaihust <58268745+khoaihust@users.noreply.github.com>
2020-04-16 09:13:02 +0700

    Update Load_Dispaly_Image.py

--- Day1/Load_Dispaly_Image.py
+++ Day1/Load_Dispaly_Image.py
@@ -1,10 +1,9 @@
-import numpy as np
 import argparse
 import cv2
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True, 
-	              help = "Path to the image")
+	            help = "Path to the image")
 args = vars(ap.parse_args())
 
 img = cv2.imread(args["image"])
