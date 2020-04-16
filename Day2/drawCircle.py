0deb7a9a0175eb5a7b1bf2d40c19a7419057d0d9
khoaihust <58268745+khoaihust@users.noreply.github.com>
2020-04-16 10:42:06 +0700

    Update drawCircle.py

--- Day2/drawCircle.py
+++ Day2/drawCircle.py
@@ -1,12 +1,20 @@
 import numpy as np
 import cv2
 
-canvas = np.zeros((300, 300, 3), dtype = 'uint8') # 300 rows, 300 columns
+canvas = np.zeros((300, 300, 3), dtype = 'uint8') # 500 rows, 700 columns
 (centerX, centerY) = (canvas.shape[1] // 2, canvas.shape[0] // 2) # centerX, center Y is x, y coordinate 
 white = (255, 255, 255)
 
 for r in range(0, 175, 25):
-  cv2.circle(canvas, (centerX, centerY), r, white)
+	cv2.circle(canvas, (centerX, centerY), r, white)
 
 cv2.imshow('circle', canvas)
+
+for i in range(0, 25):
+	radius = np.random.randint(5, high = 200)
+	color = np.random.randint(0, high = 256, size = (3,)).tolist() # list  color 3-dimensional
+	pt = np.random.randint(0, high = 300, size = (2,)) 
+	cv2.circle(canvas, tuple(pt), radius, color, -1)
+cv2.imshow('many circle', canvas)
+
 cv2.waitKey(0)
