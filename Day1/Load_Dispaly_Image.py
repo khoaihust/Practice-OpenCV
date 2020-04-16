44d718cd130a6f883f1d7d36f54e8bd359e0c0a6
khoaihust <58268745+khoaihust@users.noreply.github.com>
2020-04-16 09:09:56 +0700

    Update Load_Dispaly_Image.py

--- Day1/Load_Dispaly_Image.py
+++ Day1/Load_Dispaly_Image.py
@@ -1,9 +1,25 @@
+import numpy as np
+import argparse
 import cv2
 
-image = cv2.imread('T20.jpg')
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, 
+	              help = "Path to the image")
+args = vars(ap.parse_args())
 
-cv2.imshow("Tham", image)
+img = cv2.imread(args["image"])
+#img = cv2.imread('T20.jpg')
+
+print(type(img))
+print(img.shape)
+print('width: {} pixels'.format(img.shape[1]))
+print('height: {} pixels'.format(img.shape[0]))
+print('channels: {}'.format(img.shape[2]))
+
+cv2.imshow("Tham", img)
 cv2.waitKey(0)
+
+cv2.imwrite('newimage.jpg', img)
 cv2.destroyAllWindows()
 
 
