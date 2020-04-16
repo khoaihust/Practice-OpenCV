c395d4f0b9ae84fa283ae365b3592e90de9789c4
khoaihust <58268745+khoaihust@users.noreply.github.com>
2020-04-16 21:35:09 +0700

    Create Translation.py
    Image Transformations

---
+++ Day3/Translation.py
@@ -0,0 +1,23 @@
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required = True, help = 'Path to the image')
+args = vars(ap.parse_args())
+
+image = cv2.imread(args['image'])
+cv2.imshow('Original', image)
+
+M = np.float32([[1, 0, 25], [0, 1, 50]])
+shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+cv2.imshow('shifted down and right', shifted)
+
+M = np.float32([[1, 0, -50], [0, 1, -90]])
+shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+cv2.imshow('shifted up and left', shifted)
+
+shifted = imutils.translate(image, 0, 100)
+cv2.imshow('shifted down', shifted)
+cv2.waitKey(0)
